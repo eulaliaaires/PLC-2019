f2fe568dd9dd97c33b8b542ab96c7b7b0b1628dd
eulaliaaires <eulaliaaires@gmail.com>
2019-08-06 14:52:19 -0300

    aula 01 - 06/08

---
+++ 2019.2/aula01(06_08).hs
@@ -0,0 +1,11 @@
+-- funcaoConstante :: Integer
+funcaoConstante = 30 + 5
+
+menorQue30 :: Integer -> Bool
+menorQue30 x = x < 30
+
+quadrado :: Integer -> Integer
+quadrado x = x * x
+
+valoresIguais :: Integer -> Integer -> Integer -> Bool
+valoresIguais x y z = (x==y) && (y==z)
